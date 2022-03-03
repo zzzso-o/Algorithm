ec26bfbd42fc247bd954c299a8ccbbda51b23845
jisoo <1211ditto@gmail.com>
2022-03-03 20:25:20 +0900

    2480

--- 2/2480.py
+++ 2/2480.py
@@ -3,7 +3,9 @@ if numbers[0] != numbers[1]:
     if numbers[1] != numbers[2]:
         print(max(numbers) * 100)
     elif numbers[1] == numbers[2]:
-        print(10000 + numbers[1]*1000)
+        print(1000 + numbers[1]*100)
 elif numbers[0] == numbers[1]:
     if numbers[1] != numbers[2]:
-        print
+        print(1000 + numbers[0]*100)
+    else:
+        print(10000 + numbers[0] * 1000)
