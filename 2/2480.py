a220d71e9653960b1365697185bcdbe3b2774bca
jisoo <1211ditto@gmail.com>
2022-03-03 20:28:18 +0900

    2480_2

--- 2/2480.py
+++ 2/2480.py
@@ -1,11 +1,13 @@
 numbers = list(map(int, input().split()))
-if numbers[0] != numbers[1]:
+if numbers[0] != numbers[1] and numbers[0] != numbers[2]:
     if numbers[1] != numbers[2]:
         print(max(numbers) * 100)
-    elif numbers[1] == numbers[2]:
+    else:
         print(1000 + numbers[1]*100)
-elif numbers[0] == numbers[1]:
+elif numbers[0] == numbers[1] and numbers[0] != numbers[2]:
     if numbers[1] != numbers[2]:
         print(1000 + numbers[0]*100)
     else:
-        print(10000 + numbers[0] * 1000)
+        print(1000 + numbers[0] * 100)
+else:
+    print(10000 + numbers[0]*1000)
