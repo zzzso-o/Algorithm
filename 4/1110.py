8bfba644e7b226166e2dec151d9e1f9a049b4177
jisoo <1211ditto@gmail.com>
2022-03-13 23:36:08 +0900

    1110

--- 4/1110.py
+++ 4/1110.py
@@ -1,7 +1,12 @@
+n = int(input())
+num = n
 cnt = 0
-n = input()
-new_num = 0
-while int(n) == new_num:
-    if int(n) < 10:
-        new_num = int('0' + n)
-        
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+    cnt += 1
+    if (num == n):
+        break
