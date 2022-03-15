808751b830b06d91d9b14ebab0f831b5df1d86e5
jisoo <1211ditto@gmail.com>
2022-03-16 00:00:18 +0900

    4673solved!

--- 6/4673.py
+++ 6/4673.py
@@ -1,11 +1,19 @@
+# 생성자를 구하는 함수
 def d(n):
-    return n + (n//10) + (n%10)
+    x = 0
+    # str로 바꿔서 각 자리수 더하기
+    a = list(str(n))
+    for i in a:
+        x = x + int(i)
+    return n + x
 
-
-numbers = set(range(1, 10001))
-rmv = set()
+# set에 넣어서 중복 제거하는 방법으로 셀프넘버 찾기
+result = set()
 for i in range(1, 10001):
-    rmv.add(d(i))
-numbers = numbers - rmv
-for j in sorted(numbers):
-    print(j)
+    result.add(d(i))
+numbers = set(range(1, 10000))
+ans = numbers - result
+for k in sorted(ans):
+    print(k)
+
+
