a6c0d4c12363d980619b941e6f9c46438d67aba6
jisoo <1211ditto@gmail.com>
2022-03-20 22:21:21 +0900

    1065_í틀림

--- 6/1065.py
+++ 6/1065.py
@@ -1,15 +1,15 @@
-# 생성자를 구하는 함수
-def d(n):
-    x = 0
-    a = list(str(n))
-    for i in a:
-        x = x + int(i)
-    return n + x
+def hansu(x):
+    cnt = 0
+    if x < 100:
+        for i in range(1, x+1):
+            cnt += i
+        return cnt
+    elif x >= 100:
+        cnt = 99
+        for i in range(100, x+1):
+            if (i//100)-((i%100)//10) == ((i%100)//10)-((i%100)%10):
+                cnt += 1
+        return cnt
 
-result = 0
-for i in range(1, 10001):
-    result.add(d(i))
-numbers = set(range(1, 10000))
-ans = numbers - result
-for k in sorted(ans):
-    print(k)
+N = int(input())
+print(hansu(N))
