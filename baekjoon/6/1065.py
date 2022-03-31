2bf1d46c1e50226f92b67ef854b881f1bb2dfcfe
jisoo <1211ditto@gmail.com>
2022-03-31 21:58:59 +0900

    백준_1065_complete

--- baekjoon/6/1065.py
+++ baekjoon/6/1065.py
@@ -7,7 +7,7 @@ for i in range(1, N+1):
 
     else:
         nums = list(map(int, str(i)))
-        if nums[0] - nums[1] == nums[1] - nums[2]:
+        if nums[1] - nums[0] == nums[2] - nums[1]:
             cnt += 1
 
 print(cnt)
