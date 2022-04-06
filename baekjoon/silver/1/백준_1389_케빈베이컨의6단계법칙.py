e84682ff732c18a59f774092438ba3de91b13fa0
jisoo <1211ditto@gmail.com>
2022-04-06 21:37:52 +0900

    1389

---
+++ baekjoon/silver/1/백준_1389_케빈베이컨의6단계법칙.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = 1
+
+    while queue:
+        target = queue.popleft()
+
+        for i in graph[target]:
+            if not visited[i]:
+                visited[i] = visited[target] + 1
+                queue.append(i)
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+result = []
+
+for i in range(1, n+1):
+    visited = [0] * (n+1)
+    bfs(i)
+    result.append(sum(visited))
+
+print(result.index(min(result))+1)
