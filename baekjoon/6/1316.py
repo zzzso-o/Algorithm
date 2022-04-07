f27167956b4f788c199a3a70f224a522de402044
jisoo <1211ditto@gmail.com>
2022-04-07 23:45:39 +0900

    1316

--- baekjoon/6/1316.py
+++ baekjoon/6/1316.py
@@ -1,4 +1,10 @@
 N = int(input())
 words = [input() for _ in range(N)]
+bucket = [0] * len(words)
+x = 1
 for i in words:
-    for j in range(len(j)):
+    for j in range(len(words)):
+        if i == words[j]:
+            bucket[j] += x
+        x += 1
+print(bucket)
--- baekjoon/silver/2/백준_11725_트리의부모찾기.py
+++ baekjoon/silver/2/백준_11725_트리의부모찾기.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+
 N = int(input())
 
 graph = [[] for i in range(N+1)]
@@ -11,12 +12,12 @@ for i in range(N-1):
     graph[b].append(a)
 visited = [0] * (N+1)
 
-
 def dfs(s):
     for i in graph[s]:
         if visited[i] == 0:
             visited[i] = s
             dfs(i)
+
 dfs(1)
 for x in range(2, N+1):
     print(visited[x])
