f8c6370e970ef0403a7f341f10b0d08f5765792d
jisoo <1211ditto@gmail.com>
2022-04-12 00:08:06 +0900

    2606_다시풀기

--- baekjoon/silver/3/백준_2606_바이러스.py
+++ baekjoon/silver/3/백준_2606_바이러스.py
@@ -2,15 +2,14 @@ n = int(input())
 m = int(input())
 graph = [[]*n for _ in range(n+1)]
 
-
-
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
+# print(graph)
 
 cnt = 0
-visited = [0]*(n+1)
+visited = [0] * (n+1)
 def dfs(start):
     global cnt
     visited[start] = 1
