6cc03b7dbd1adcc5311bb0e293e8754f09a0bd29
jisoo <1211ditto@gmail.com>
2022-04-14 23:49:55 +0900

    2869_왜안됨?

--- baekjoon/7/2869.py
+++ baekjoon/7/2869.py
@@ -1,10 +1,14 @@
 A, B, V = map(int, input().split())
 day = 0
 i = 0
-while i == V:
-    i + A
-    day + 0.5
-    i - B
-    day + 0.5
+while True:
+    i += A
+    day += 0.5
+    if i >= V:
+        break
+    i -= B
+    day += 0.5
+    if i >= V:
+        break
 
 print(round(day))
