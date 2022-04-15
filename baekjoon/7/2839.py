de2ebe9af62b5add0020f496b7ff5ee96737fa99
jisoo <1211ditto@gmail.com>
2022-04-16 00:33:09 +0900

    2839_again

--- baekjoon/7/2839.py
+++ baekjoon/7/2839.py
@@ -1,9 +1,14 @@
 N = int(input())
-
-i = 1
-while i <= N:
+cnt = 0
+while N >= 0:
     if N % 5 == 0:
-        print(N//5)
-    elif N % 5 != 0:
-        N -= 5*i
+        cnt += (N //5)
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+else:
+    print(-1)
 
+# 난 왜 이런 코드를 생각하지 못할까..
+# 조금만 더 생각하면 생각이 날 법도 한데..
