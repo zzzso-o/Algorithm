b6849d6b11d5baa8a45924fb3d3894211ad2bc09
jisoo <1211ditto@gmail.com>
2022-04-24 23:54:56 +0900

    3009_complete

--- baekjoon/8/3009.py
+++ baekjoon/8/3009.py
@@ -1,9 +1,12 @@
 arr = [list(map(int, input().split())) for _ in range(3)]
 ans = []
 
-for i in range(3):
-    if arr[0][0] - arr[i][0] != 0 or arr[1][0] - arr[i][0] != 0 or arr[2][0] - arr[i][0] != 0:
-        ans.append(arr[i][0])
-    elif arr[0][1] - arr[i][1] != 0 or arr[1][1] - arr[i][1] != 0 or arr[2][1] - arr[i][1] != 0:
-        ans.append(arr[i][1])
-print(ans)
+for i in range(2):
+    if arr[0][i] != arr[1][i] and arr[0][i] != arr[2][i]:
+        ans.append(arr[0][i])
+    elif arr[1][i] != arr[0][i] and arr[1][i] != arr[2][i]:
+        ans.append(arr[1][i])
+    elif arr[2][i] != arr[0][i] and arr[2][i] != arr[1][i]:
+        ans.append(arr[2][i])
+
+print(*ans)
