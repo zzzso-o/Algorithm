d93818594cf11f9557a7ed819ce79acda4631ef5
jisoo <1211ditto@gmail.com>
2022-05-05 23:58:29 +0900

    11403

--- baekjoon/silver/3/백준_2910_빈도 정렬.py
+++ baekjoon/silver/3/백준_2910_빈도 정렬.py
@@ -1,3 +1,9 @@
-N, C = map(int, input().split())
+n, c = map(int, input().split())
 numbers = list(map(int, input().split()))
 
+new_list = []
+bucket = [0] * n
+for i in range(n):
+    for j in list(set(numbers)):
+        if j == numbers[i]:
+            bucket[i] += 1
