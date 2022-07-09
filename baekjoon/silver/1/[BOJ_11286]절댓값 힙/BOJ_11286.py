de77ab0fc1993f941185c719eeee04d3c7b19a9a
zzzso-o <1211ditto@gmail.com>
2022-07-09 21:18:35 +0900

    boj_11286

--- baekjoon/silver/1/[BOJ_11286]절댓값 힙/BOJ_11286.py
+++ baekjoon/silver/1/[BOJ_11286]절댓값 힙/BOJ_11286.py
@@ -1,2 +1,18 @@
-import sys 
+import sys, heapq
 sys.stdin = open('input1.txt') 
+# input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+
+    else:
+        heapq.heappush(heap, (abs(x), x))
