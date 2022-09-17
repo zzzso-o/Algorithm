509025ead1818aec589f8d10f102112f9bb029f3
zzzso-o <1211ditto@gmail.com>
2022-09-17 23:44:21 +0900

    [PRO] 같은 숫자는 싫어 레벨1 10min

---
+++ programmers/같은 숫자는 싫어/이지수.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]:
+            answer.append(arr[i])
+        else:
+            pass
+    answer.append(arr[-1])
+    return answer
