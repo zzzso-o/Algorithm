74030c6c6bc6517bfff28578bc473d5427e8bf84
zzzso-o <1211ditto@gmail.com>
2023-01-11 00:18:17 +0900

    [BOJ_1181]단어정렬_30m_R

--- baekjoon/[BOJ_1181]단어 정렬/BOJ_1181.py
+++ baekjoon/[BOJ_1181]단어 정렬/BOJ_1181.py
@@ -1,5 +1,14 @@
 import sys 
-sys.stdin = open('input1.txt') 
+sys.stdin = open('input1.txt')
+
 
 N = int(input())
-words = list()
+words = []
+for i in range(N):
+    words.append(input())
+
+clear = list(set(words))
+result = sorted(clear)
+result = sorted(result, key=len)
+for i in result:
+    print(i)
