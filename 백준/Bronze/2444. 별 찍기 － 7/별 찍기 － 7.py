8a0e1a810c8b0adb3e83ca198fa098caea812864
Jisoo LEE <1211ditto@gmail.com>
2023-04-23 23:22:41 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,7 @@
+import sys 
+# sys.stdin = open('input1.txt')
+n = int(input())
+for i in range(1, n):
+    print(' '*(n-i) + '*'*(2*i-1))
+for i in range(n, 0, -1):
+    print(' '*(n-i) + '*'*(2*i-1))
